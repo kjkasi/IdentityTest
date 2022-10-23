54365a9bf287b00d6719bf300a67b07938c8a8b4
kjkasi <5842602+kjkasi@users.noreply.github.com>
2022-10-23 16:21:27 +0300

    no message

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -1,4 +1,5 @@
 ﻿using Duende.IdentityServer.Models;
+using static System.Net.WebRequestMethods;
 
 namespace IdentityServer
 {
@@ -23,7 +24,7 @@ namespace IdentityServer
                 new Client
                 {
                     ClientId = "client",
-                    ClientSecrets = { new Secret("secret") },
+                    ClientSecrets = { new Secret("secret".Sha256()) },
 
                     AllowedGrantTypes = GrantTypes.Code,
                     AllowedScopes = { "openid", "api1" }
@@ -31,9 +32,21 @@ namespace IdentityServer
                 new Client
                 {
                     ClientId = "mobile",
-                    ClientSecrets = { new Secret("secret") },
+                    ClientSecrets = { new Secret("secret".Sha256()) },
                     AllowedGrantTypes = GrantTypes.Code,
-
+                    RedirectUris = { "https://host.docker.internal:5001/signin-oidc" },
+                    AllowedScopes = new List<string>
+                    {
+                        "openid",
+                        "api1"
+                    }
+                },
+                new Client
+                {
+                    ClientId = "device",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.DeviceFlow,
+                    RedirectUris = { "http://localhost:5000/signin-oidc" },
                     AllowedScopes = new List<string>
                     {
                         "openid",
--- IdentityServer/HostingExtensions.cs
+++ IdentityServer/HostingExtensions.cs
@@ -9,11 +9,7 @@ namespace IdentityServer
             // uncomment if you want to add a UI
             //builder.Services.AddRazorPages();
 
-            builder.Services.AddIdentityServer(options =>
-                {
-                    // https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/api_scopes#authorization-based-on-scopes
-                    options.EmitStaticAudienceClaim = true;
-                })
+            builder.Services.AddIdentityServer()
                 .AddInMemoryIdentityResources(Config.IdentityResources)
                 .AddInMemoryApiScopes(Config.ApiScopes)
                 .AddInMemoryClients(Config.Clients);
